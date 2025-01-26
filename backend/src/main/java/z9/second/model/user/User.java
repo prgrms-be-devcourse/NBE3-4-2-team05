ed787337b4569206e8b8c5aa0a1405e3d761c71c
KangBaekGwa <ksu9801@gmail.com>
2025-01-26 15:37:47 +0900

    feat : user entity 생성

---
+++ backend/src/main/java/z9/second/model/user/User.java
@@ -0,0 +1,61 @@
+package z9.second.model.user;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import z9.second.model.BaseEntity;
+
+@Entity
+@Getter
+@ToString
+@Table(name = "users")
+@Builder(access = AccessLevel.PRIVATE)
+@EqualsAndHashCode(callSuper = false)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User extends BaseEntity {
+
+    @Id
+    @Column(name = "user_id")
+    @GeneratedValue
+    private Long id;
+
+    @Column(name = "login_id", nullable = false, unique = true)
+    private String loginId;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "nickname", nullable = false, unique = true, length = 10)
+    private String nickname;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status", nullable = false)
+    private UserStatus status;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role", nullable = false)
+    private UserRole role;
+
+    private static User createNewUser(String loginId, String password, String nickname) {
+        return User
+                .builder()
+                .loginId(loginId)
+                .password(password)
+                .nickname(nickname)
+                .status(UserStatus.ACTIVE)
+                .role(UserRole.ROLE_USER)
+                .build();
+    }
+}
