fa1c92fc82ecad401dc07da8f8ab2d993515bbd9
davinyakma <poohcdv3652@naver.com>
2025-01-31 12:18:18 +0900

    Feat: Schedules DTO 추가
    -SchedulesRequestDto, SchedulesResponseDto 수정
    -CheckInRequestDto, CheckInResponseDto 추가

---
+++ backend/src/main/java/z9/second/domain/schedules/dto/CheckInResponseDto.java
@@ -0,0 +1,42 @@
+package z9.second.domain.schedules.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import z9.second.model.schedules.SchedulesCheckInEntity;
+
+public class CheckInResponseDto {
+    @Getter
+    @Builder
+    @AllArgsConstructor
+    public static class ResponseData {
+        private final Long checkInId;
+        private final Long scheduleId;
+        private final Long userId;
+        private final Boolean checkIn;
+
+        public static ResponseData from(SchedulesCheckInEntity schedulesCheckInEntity) {
+            return ResponseData.builder()
+                    .checkInId(schedulesCheckInEntity.getId())
+                    .scheduleId(schedulesCheckInEntity.getSchedules().getId())
+                    .userId(schedulesCheckInEntity.getUserId())
+                    .checkIn(schedulesCheckInEntity.isCheckIn())
+                    .build();
+        }
+    }
+
+    @Getter
+    @Builder
+    @AllArgsConstructor
+    public static class ListResponseData {   // 참석자 목록 조회를 위한 응답 DTO
+        private final Long userId;
+        private final Boolean checkIn;
+
+        public static ListResponseData from(SchedulesCheckInEntity entity) {
+            return ListResponseData.builder()
+                    .userId(entity.getUserId())
+                    .checkIn(entity.isCheckIn())
+                    .build();
+        }
+    }
+}
--- backend/src/main/java/z9/second/domain/schedules/dto/SchedulesRequestDto.java
+++ backend/src/main/java/z9/second/domain/schedules/dto/SchedulesRequestDto.java
@@ -1,28 +1,26 @@
 package z9.second.domain.schedules.dto;
 
 import jakarta.validation.constraints.NotNull;
-import lombok.*;
-import z9.second.model.schedules.SchedulesEntity;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
-@Getter
-@Builder
-@AllArgsConstructor
-@NoArgsConstructor
 public class SchedulesRequestDto {
-    @NotNull(message = "Class ID must not be null")
-    private Long classId;
+    @Getter
+    @Builder
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class RequestData {
+        @NotNull(message = "Class ID must not be null")
+        private Long classId; // 일정을 생성할 클래스의 ID
 
-    @NotNull(message = "meeting_time must not be null")
-    private String meetingTime;
+        @NotNull(message = "meeting_time must not be null")
+        private String meetingTime;
 
-    @NotNull(message = "meeting_title must not be null")
-    private String meetingTitle;
-
-    public static SchedulesRequestDto of(SchedulesEntity schedules) {
-        return  SchedulesRequestDto.builder()
-                .classId(schedules.getId())
-                .meetingTime(schedules.getMeetingTime())
-                .meetingTitle(schedules.getMeetingTitle())
-                .build();
+        @NotNull(message = "meeting_title must not be null")
+        @Size(min = 2, message = "모임 제목은 2글자 이상이어야 합니다.")
+        private String meetingTitle;
     }
 }
