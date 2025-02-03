f7c3b4b8fcf8368071ecbcaced6c14779e78047a
davinyakma <poohcdv3652@naver.com>
2025-02-03 14:21:17 +0900

    Feat: CustomException 추가
    - 모임 일정 생성, 실패 메세지 추가

--- backend/src/main/java/z9/second/domain/schedules/service/SchedulesService.java
+++ backend/src/main/java/z9/second/domain/schedules/service/SchedulesService.java
@@ -6,6 +6,8 @@ import z9.second.domain.classes.entity.ClassEntity;
 import z9.second.domain.classes.repository.ClassRepository;
 import z9.second.domain.schedules.dto.SchedulesRequestDto;
 import z9.second.domain.schedules.dto.SchedulesResponseDto;
+import z9.second.global.exception.CustomException;
+import z9.second.global.response.ErrorCode;
 import z9.second.model.schedules.SchedulesEntity;
 import z9.second.model.schedules.SchedulesRepository;
 
@@ -16,22 +18,32 @@ public class SchedulesService {
     private final ClassRepository classesRepository;
 
     //생성
-    public SchedulesResponseDto.ResponseData create(SchedulesRequestDto.RequestData requestData) {
+    public void create(SchedulesRequestDto.RequestData requestData, Long userId) {
         // 1. 요청된 classId로 실제 클래스가 있는지 확인
         ClassEntity classes = classesRepository.findById(requestData.getClassId())
-                .orElseThrow(() -> new IllegalArgumentException("Class not found"));
+                .orElseThrow(() -> new CustomException(ErrorCode.CLASS_NOT_FOUND));
 
-        // 2. 새로운 일정 엔티티 생성
-        SchedulesEntity schedules = SchedulesEntity.builder()
-                .classes(classes)                          // 찾은 클래스 정보
-                .meetingTime(requestData.getMeetingTime()) // 모임 시간
-                .meetingTitle(requestData.getMeetingTitle()) // 모임 제목
-                .build();
+        // 모임장 권한 체크
+        if (!classes.getMasterId().equals(userId)) {
+            throw new CustomException(ErrorCode.CLASS_ACCESS_DENIED);
+        }
 
-        // 3. DB에 저장하고
-        SchedulesEntity savedSchedule = schedulesRepository.save(schedules);
+        try {
+            // 2. 새로운 일정 엔티티 생성
+            SchedulesEntity schedules = SchedulesEntity.builder()
+                    .classes(classes)                          // 찾은 클래스 정보
+                    .meetingTime(requestData.getMeetingTime()) // 모임 시간
+                    .meetingTitle(requestData.getMeetingTitle()) // 모임 제목
+                    .build();
 
-        // 4. 저장된 결과를 ResponseDto로 변환해서 반환
-        return SchedulesResponseDto.ResponseData.from(savedSchedule);
+            // 3. DB에 저장하고
+            SchedulesEntity savedSchedule = schedulesRepository.save(schedules);
+
+            // 4. 저장된 결과를 ResponseDto로 변환해서 반환
+            SchedulesResponseDto.ResponseData.from(savedSchedule);
+        } catch (Exception e) {
+            throw new CustomException(ErrorCode.SCHEDULE_CREATE_FAILED);
+        }
+
     }
 }
