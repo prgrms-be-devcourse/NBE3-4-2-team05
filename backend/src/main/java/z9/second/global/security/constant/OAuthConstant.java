d8ee0d98fcd65c070731a120539441a0c7ddf40f
KangBaekGwa <ksu9801@gmail.com>
2025-02-03 10:11:51 +0900

    feat : 소셜 로그인 기능 일부 구현 - 카카오 로그인만 우선적으로 개발

---
+++ backend/src/main/java/z9/second/global/security/constant/OAuthConstant.java
@@ -0,0 +1,7 @@
+package z9.second.global.security.constant;
+
+public abstract class OAuthConstant {
+    public static final String OAUTH_PROVIDER_GOOGLE = "google";
+    public static final String OAUTH_PROVIDER_NAVER = "naver";
+    public static final String OAUTH_PROVIDER_KAKAO = "kakao";
+}
---
+++ backend/src/main/java/z9/second/model/oauthuser/OAuthProvider.java
@@ -0,0 +1,10 @@
+package z9.second.model.oauthuser;
+
+import lombok.Getter;
+
+@Getter
+public enum OAuthProvider {
+    KAKAO,
+    GOOGLE,
+    NAVER,
+}
