a86e4cb323d44a302aba637f1fc99828334756b0
KangBaekGwa <ksu9801@gmail.com>
2025-02-05 15:41:28 +0900

    feat : favoriteEntity 와 매핑 관계 기본 로딩 전략 fetch-lazy 로 변경

--- backend/src/main/java/z9/second/model/userfavorite/UserFavorite.java
+++ backend/src/main/java/z9/second/model/userfavorite/UserFavorite.java
@@ -2,6 +2,7 @@ package z9.second.model.userfavorite;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -37,7 +38,7 @@ public class UserFavorite {
     @JoinColumn(name = "user_id")
     private User user;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "favorite_id")
     private FavoriteEntity favorite;
 
