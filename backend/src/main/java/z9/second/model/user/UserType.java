0c70b043ec33d7bf1c1faa6903106809e6a3831d
KangBaekGwa <ksu9801@gmail.com>
2025-02-05 15:42:12 +0900

    feat : userRole, userType enum 타입 요소 별 값 추가

--- backend/src/main/java/z9/second/model/user/UserType.java
+++ backend/src/main/java/z9/second/model/user/UserType.java
@@ -1,7 +1,14 @@
 package z9.second.model.user;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum UserType {
-    NORMAL,
-    OAUTH
-    ,
+    NORMAL("일반 회원"),
+    OAUTH("소셜 회원"),
+    ;
+
+    private final String value;
 }
