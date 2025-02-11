b8afef688ea4fa02398ca205e099a888a06301d6
jho951 <jho951@naver.com>
2025-02-11 10:48:04 +0900

    Fix: conflict

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ReactDOM from "react-dom/client";
-
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import SignUp from "./pages/signUp/SignUp";
@@ -11,7 +10,6 @@ import Mypage from "./pages/mypage/Mypage";
 import Modify from "./pages/mypage/Modify";
 import KakaoCallback from "./components/kakao/KakaoCallback";
 import MemberList from "./pages/class/[id]/MemberList";
-
 import Layout from "./pages/layout/Layout";
 import ScheduleDetail from "./pages/schedules/[id]/ScheduleDetail";
 
@@ -26,7 +24,6 @@ const App = () => {
           <Route path="/modify" element={<Modify />} />
           <Route path="/join" element={<SignUp />} />
           <Route path="/mypage" element={<Mypage />} />
-          <Route path="/classes" element={<Class />} />
           <Route path="/classes/:id" element={<Class />} />
           <Route path="/kakaomap" element={<KakaoMap />} />
           <Route path="/memberList/:id" element={<MemberList />} />
